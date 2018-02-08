f408d51cf76667af557470c710fe26980aa5710f
Eddie <dcwen199366@gmail.com>
2018-02-08 18:35:45 +0800

    异常日志及其线上模式的异常提醒

--- data/conf/config.php
+++ data/conf/config.php
@@ -1,19 +1,14 @@
 <?php
 return [
-    'app_debug' =>true,
+    'app_debug' =>false,
     //异常页面,调试模式关闭的时候显示模板
     //路由找不到获取逻辑错误都会跳转到这个页面
-    'exception_tmpl'         => ROOT_PATH.'public'.DS.'admin/alpha/pages-error-404-2.html',
+    'exception_tmpl'         => ROOT_PATH.'public'.DS.'admin/alpha/pages-error-500.html',
     // 关闭URL中控制器和操作名的自动转换
     'url_convert'    =>  false,
     // 默认模块名,控制器
     'default_module' => 'admin',
     'default_controller' => 'core.Index',
-    //系统日志
-    'log'   => [
-        //日志保存目录
-        'path' =>  RUNTIME_PATH.'log'.DS.'system'.DS,
-    ],
     // 视图输出字符串内容替换
     'view_replace_str' => [
         '__ROOT__' => $basename,
