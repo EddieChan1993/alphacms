9141bdc396862a78d1d84725d321a36be29e0f55
Eddie <dcwen199366@gmail.com>
2018-03-01 10:27:38 +0800

    优化

--- data/conf/config.php
+++ data/conf/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'app_debug' => false,
+    'app_debug' => true,
     //异常页面,调试模式关闭的时候显示模板
     //路由找不到获取逻辑错误都会跳转到这个页面
     'exception_tmpl' => ROOT_PATH . 'public' . DS . 'admin/alpha/pages-error-500.html',
